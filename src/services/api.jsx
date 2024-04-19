c9b11db2e26e3820ea3437e66f430f35a038c311
Braulio <braulioecheverria@kinal.org.gt>
2024-04-19 10:48:29 -0600

    login form and register done

---
+++ src/services/api.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: 'http://127.0.0.1:3001/twitch/v1',
+    timeout: 1000
+})
+
+export const login = async (data) => {
+    try{
+        return await apiClient.post('/auth/login', data)
+    }catch(e){
+        return{
+            error: true,
+            e
+        }
+    }
+}
+
+export const register = async (data) => {
+    try{
+        return await apiClient.post('/auth/register', data)
+    }catch(e){
+        return{
+            error: true,
+            e
+        }
+    }
+}
